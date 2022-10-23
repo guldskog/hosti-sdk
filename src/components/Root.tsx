757d01d71d6ac2d3b86a6c697085a6662040a409
Joacim Guldskog <joacim@sjosatta.se>
2022-10-23 10:23:47 +0200

    Home 0.1.0

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { NavigationContext } from "../../host/contexts/navigation";
 
 export interface RootProps {}
 
 export const Root: React.FC<RootProps> = (props) => {
+  const navigation = React.useContext(NavigationContext);
+
   return (
-    <div className="grid place-items-center h-full w-full absolute">
-      <div className="text-2xl text-center">My App</div>
+    <div className="absolute w-full h-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <div className="grid grid-cols-7 gap-20 py-40 px-10 max-w-7xl mx-auto">
+        <a href="/app-store">
+          <button className="w-full aspect-square bg-blue-500 rounded-2xl shadow-lg"></button>
+          <div className="overflow-hidden whitespace-nowrap text-ellipsis text-white drop-shadow text-sm text-center">
+            App Store
+          </div>
+        </a>
+      </div>
     </div>
   );
 };
