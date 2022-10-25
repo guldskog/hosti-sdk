7974245a4f8f1525fca712026da85b3311390a11
Joacim Guldskog <joacim@sjosatta.se>
2022-10-25 21:27:14 +0200

    Add "To Do" app

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -9,10 +9,10 @@ export const Root: React.FC<RootProps> = (props) => {
   return (
     <div className="absolute w-full h-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
       <div className="grid grid-cols-7 gap-20 py-40 px-10 max-w-7xl mx-auto">
-        <a href="/app-store">
+        <a href="/to-do">
           <button className="w-full aspect-square bg-blue-500 rounded-2xl shadow-lg"></button>
           <div className="overflow-hidden whitespace-nowrap text-ellipsis text-white drop-shadow text-sm text-center">
-            App Store
+            To Do
           </div>
         </a>
       </div>
