27701986282e5c767d2c96e594ac48a49abd06e1
Joacim Guldskog <joacim@guldskog.se>
2022-11-18 22:18:15 +0100

    Version 2

--- src/components/AppList.tsx
+++ src/components/AppList.tsx
@@ -3,22 +3,15 @@ import React, { useEffect } from "react";
 
 export interface AppListProps {}
 
-interface App {
-  id: string;
-  name: string;
-  author: Author;
-}
-
-interface Author {
-  name: string;
-  avatarUrl: string;
-  htmlUrl: string;
-}
-
 export const AppList: React.FC<AppListProps> = (props) => {
   const navigation = React.useContext(NavigationContext);
 
-  const [apps, setApps] = React.useState<App[]>([]);
+  const [apps, setApps] = React.useState<
+    {
+      id: string;
+      name: string;
+    }[]
+  >([]);
 
   useEffect(() => {
     const fetchBranches = async () => {
@@ -32,34 +25,23 @@ export const AppList: React.FC<AppListProps> = (props) => {
     (async () => {
       const branches = await fetchBranches();
       const appsBranches = branches.filter(
-        (branch) => branch.name.includes("app") && branch.name !== "apps/home"
+        (branch) => branch.name.includes("apps") && branch.name !== "apps/home"
       );
 
-      const apps: App[] = [];
+      console.log(appsBranches);
 
-      for (const appBranch of appsBranches) {
-        const branch = await fetch(
-          `https://api.github.com/repos/guldskog/hosti-sdk/branches/${appBranch.name}`
-        ).then((response) => response.json());
-
-        apps.push({
+      setApps(
+        appsBranches.map((appBranch) => ({
           id: appBranch.name,
           name: appBranch.name,
-          author: {
-            name: branch.commit.author?.login,
-            avatarUrl: branch.commit.author?.avatar_url,
-            htmlUrl: branch.commit.author?.html_url,
-          },
-        });
-      }
-
-      setApps(apps);
+        }))
+      );
     })();
   }, []);
 
   return (
-    <div className="mt-6 flow-root px-6">
-      <ul role="list" className="-my-5 divide-y divide-gray-200">
+    <div className="mt-6 flow-root px-6 w-full max-w-lg">
+      <ul role="list" className="-my-5 divide-y divide-gray-900">
         {apps.length === 0 ? (
           <div role="status">
             <svg
@@ -85,33 +67,17 @@ export const AppList: React.FC<AppListProps> = (props) => {
             const appId = app.name.replace("apps/", "");
 
             return (
-              <li
-                key={app.id}
-                className="p-4 hover:bg-indigo-50 transition duration-100"
-              >
+              <li key={app.id} className="p-4">
                 <div className="flex items-center space-x-4">
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-8 w-8 rounded-full"
-                      src={app.author.avatarUrl}
-                      alt=""
-                    />
-                  </div>
                   <div className="min-w-0 flex-1">
-                    <p className="truncate text-sm font-medium text-gray-900">
+                    <p className="truncate text-sm font-medium text-white">
                       {appId}
                     </p>
-                    <a
-                      href={app.author.htmlUrl}
-                      className="truncate text-sm text-gray-500"
-                    >
-                      @{app.author.name}
-                    </a>
                   </div>
                   <div>
                     <button
                       onClick={() => navigation.navigate({ appId: appId })}
-                      className="inline-flex items-center rounded-full border border-gray-300 bg-white px-2.5 py-0.5 text-sm font-medium leading-5 text-gray-700 shadow-sm hover:bg-gray-50"
+                      className="inline-flex items-center rounded-full bg-gray-900 px-2.5 py-0.5 text-sm font-medium leading-5 text-white"
                     >
                       View
                     </button>
--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -5,12 +5,5 @@ import CardHeading from "./CardHeading";
 export interface RootProps {}
 
 export const Root: React.FC<RootProps> = (props) => {
-  return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-      <div className="mx-auto max-w-2xl">
-        <CardHeading />
-        <AppList />
-      </div>
-    </div>
-  );
+  return <AppList />;
 };
