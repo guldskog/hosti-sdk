743cf946b5d24cc46680c498854b32690dca17f8
Joacim Guldskog <joacim@guldskog.se>
2022-11-18 21:55:26 +0100

    Version 2

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -3,5 +3,11 @@ import React from "react";
 export interface RootProps {}
 
 export const Root: React.FC<RootProps> = (props) => {
-  return <>Hello!</>;
+  return (
+    <section className="w-screen h-screen grid place-items-center bg-neutral-900">
+      <h1 className="text-8xl text-transparent bg-clip-text bg-gradient-to-tr from-purple-600 font-bold to-pink-600 hover:rotate-[360deg] transition duration-100">
+        DEMO
+      </h1>
+    </section>
+  );
 };
