1adad49f3aeb953129624989d3e1c64adfdb8104
Joacim Guldskog <joacim@guldskog.se>
2022-11-18 21:57:04 +0100

    Version 1

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Seed } from "./Seed";
 
 export interface RootProps {}
 
 export const Root: React.FC<RootProps> = (props) => {
-  return <>Hello!</>;
+  return <Seed />;
 };
