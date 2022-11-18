3861b9db78864f8f840ddc4cadd75b0a4e2b1980
Joacim Guldskog <joacim@guldskog.se>
2022-11-18 21:56:11 +0100

    Version 0

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -3,5 +3,9 @@ import React from "react";
 export interface RootProps {}
 
 export const Root: React.FC<RootProps> = (props) => {
-  return <>Hello!</>;
+  return (
+    <div className="grid place-items-center h-full w-full absolute">
+      <h1>WELCOME TO JANNEJÖNNY</h1>
+    </div>
+  );
 };
