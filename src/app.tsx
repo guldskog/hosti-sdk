2192199e335696f2dbb2fd0ae9c198c87d217348
Joacim Guldskog <joacim@guldskog.se>
2022-11-18 21:54:41 +0100

    React

---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+import { NavigationContext } from "./contexts/navigation";
+import { App } from "@hosti/models";
+import ReactDOM from "react-dom/client";
+import { Root } from "./components/Root";
+import "./styles.css";
+
+const app: App = ({ node, navigation }) => {
+  const root = ReactDOM.createRoot(node);
+
+  root.render(
+    <NavigationContext.Provider value={{ ...navigation }}>
+      <Root />
+    </NavigationContext.Provider>
+  );
+};
+
+export default app;
